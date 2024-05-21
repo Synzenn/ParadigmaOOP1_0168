997b50f67437b08e81c13f6ce85ec4b58abefec8
Synzenn <velriel42@gmail.com>
2024-05-21 11:08:33 +0700

    membenarkan dan memanggil header manusia dan jantung

--- Part2/Part2.cpp
+++ Part2/Part2.cpp
@@ -2,12 +2,15 @@
 #include <string>
 using namespace std;
 
-#include 
-#include 
+#include "jantung.h"
+#include "manusia.h"
 
 
 int main()
 {
-   
+	manusia* varManusia = new manusia("Jono");
+	delete varManusia;
+	return 0;
+
 }
 
